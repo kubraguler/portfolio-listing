cbda7ad11008eec1a392aa9f10bd017e7fc50ee4
kubraguler <oktnkbr@gmail.com>
2023-08-30 23:47:47 +0100

    Edit portfolio header component

---
+++ src/components/PortfolioHeader/PortfolioHeader.tsx
@@ -0,0 +1,25 @@
+import { FunctionComponent } from "react";
+import styles from "./PortfolioHeader.module.scss";
+
+export interface PortfolioHeaderProps {
+  title: string;
+  valuation: number;
+}
+
+export const PortfolioHeader: FunctionComponent<PortfolioHeaderProps> = ({
+  title,
+  valuation,
+}: PortfolioHeaderProps) => {
+  const gbpFormatter = new Intl.NumberFormat("en-UK", {
+    style: "currency",
+    currency: "GBP",
+  });
+
+  return (
+    <div className={styles.header}>
+      <h1 className={styles.title}>{title}</h1>
+      <p className={styles["valuation-label"]}>Valuation</p>
+      <span className={styles.valuation}>{gbpFormatter.format(valuation)}</span>
+    </div>
+  );
+};
