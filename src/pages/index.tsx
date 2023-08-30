2134282a0cf2a7ac1b230cfa7ec77dbebc19ea21
kubraguler <oktnkbr@gmail.com>
2023-08-30 23:54:24 +0100

    Get data and send to components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,41 @@
 import Head from "next/head";
-import { Welcome } from "@/components/welcome/welcome";
+import { useEffect, useState } from "react";
+import { Portfolio } from "@/models/portfolio";
+import {
+  PortfolioHeader,
+  PortfolioHeaderProps,
+} from "@/components/PortfolioHeader/PortfolioHeader";
+import { PortfolioContent } from "@/components/PortfolioContent/PortfolioContent";
+
+import styles from "../styles/styles.module.scss";
 
 export default function Home() {
+  const [portfolioData, setPortfolioData] = useState<Portfolio | null>(null);
+
+  useEffect(() => {
+    fetch("api/portfolio/1")
+      .then((response) => response.json())
+      .then((data) => {
+        setPortfolioData(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  const headerValues = (): PortfolioHeaderProps => {
+    const total =
+      portfolioData?.holdings.reduce(
+        (prevHolding, currentHolding) => prevHolding + currentHolding.lastPrice,
+        0,
+      ) || 0;
+
+    return {
+      title: portfolioData?.name || "",
+      valuation: total,
+    };
+  };
+
   return (
     <>
       <Head>
@@ -11,7 +45,15 @@ export default function Home() {
           content="Stockopedia Challenge - Front-end Technical Test"
         />
       </Head>
-      <Welcome />
+      <section className={styles.portfolio}>
+        <PortfolioHeader
+          title={headerValues().title}
+          valuation={headerValues().valuation}
+        />
+        {portfolioData && (
+          <PortfolioContent holdings={portfolioData.holdings} />
+        )}
+      </section>
     </>
   );
 }
