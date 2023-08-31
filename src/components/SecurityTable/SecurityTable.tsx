bd22944beeb43de956a47511d36a0d01fa5d0d5a
kubraguler <oktnkbr@gmail.com>
2023-08-31 20:49:20 +0100

    Edit security table comp style by design

--- src/components/SecurityTable/SecurityTable.tsx
+++ src/components/SecurityTable/SecurityTable.tsx
@@ -9,29 +9,49 @@ export interface SecurityTableProps {
 export const SecurityTable: FunctionComponent<SecurityTableProps> = ({
   holdings,
 }) => {
+  const gbpFormatter = new Intl.NumberFormat("en-UK", {
+    style: "currency",
+    currency: "GBP",
+  });
   return (
     <div className={styles.table}>
-      <div className={styles.header}>
+      <div className={styles.head}>
         <div className={styles.row}>
-          <div className="header">Name</div>
-          <div className="header">Sector</div>
-          <div className="header">Shares</div>
-          <div className="header">Last Price</div>
-          <div className="header">Change</div>
+          <div className={styles.header}>Name</div>
+          <div className={styles.header}>Sector</div>
+          <div className={styles.header + " " + styles.align__right}>
+            Shares
+          </div>
+          <div className={styles.header + " " + styles.align__right}>
+            Last Price
+          </div>
+          <div className={styles.header + " " + styles.align__right}>
+            Change
+          </div>
         </div>
       </div>
       <div className={styles.body}>
         {holdings.map((holding) => (
           <div key={holding.id} className={styles.row}>
-            <div>{holding.name}</div>
-            <div>{holding.sector.name}</div>
-            <div>{holding.shares}</div>
-            <div>{holding.lastPrice}</div>
+            <div className={styles.cell}>{holding.name}</div>
+            <div className={styles.cell}>{holding.sector.name}</div>
+            <div className={styles.cell + " " + styles.align__right}>
+              {holding.shares.toLocaleString()}
+            </div>
+            <div className={styles.cell + " " + styles.align__right}>
+              {holding.lastPrice.toFixed(2)}p
+            </div>
             <div
-              className={styles[holding.change < 0 ? "negative" : "positive"]}
+              className={
+                styles.cell +
+                " " +
+                styles.align__right +
+                " " +
+                styles[holding.change < 0 ? "negative" : "positive"]
+              }
             >
               {holding.change > 0 && "+"}
-              {holding.change}
+              {holding.change.toFixed(2)}
             </div>
           </div>
         ))}
